50acdc2e255984c8ece3f15860cbe33fdbe60466
Milind Mishra <milind.mishra4@gmail.com>
2021-08-04 07:29:29 +0530

    refactor

--- assignment-solutions/2. First Java Program/Q3.java
+++ assignment-solutions/2. First Java Program/Q3.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
@@ -7,6 +6,5 @@ public class Q3 {
         System.out.print("Please enter your name: ");
         usersName = sc.nextLine();
         System.out.println("Hello, " + usersName + ", greetings from command line!");
-    
     }
 }
--- assignment-solutions/2. First Java Program/Q5.java
+++ assignment-solutions/2. First Java Program/Q5.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
