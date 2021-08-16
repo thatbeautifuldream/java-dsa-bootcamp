44f54d15d9326ed219f23216649dca7c5d83cb65
Milind Mishra <milind.mishra4@gmail.com>
2021-08-17 01:02:18 +0530

    updates

---
+++ Arrays/KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,17 @@
+// Problem Link : https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+package Arrays;
+
+public class KidsWiththeGreatestNumberofCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        
+        int max = -1;
+        for(int i=0;i<candies.length;i++){
+                max = Math.max(max,candies[i]);
+            }
+        for(int i=0;i<candies.length;i++){
+            result.add((candies[i]+extraCandies)>=max);
+        }        
+        return result;
+    }    
+}
