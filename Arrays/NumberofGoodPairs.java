4bea407311f3d67901d7bc7f636552cc67d90309
Milind Mishra <milind.mishra4@gmail.com>
2021-08-17 01:28:10 +0530

    good pairs

---
+++ Arrays/NumberofGoodPairs.java
@@ -0,0 +1,16 @@
+// Problem Link : https://leetcode.com/problems/number-of-good-pairs/
+package Arrays;
+
+public class NumberofGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i = 0; i<nums.length; i++) {
+            for(int j = i+1; j<nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
