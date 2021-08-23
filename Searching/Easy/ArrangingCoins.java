04aac2b55fc5ee6ac2a639bca8bab8fb0420ef77
Milind Mishra <milind.mishra4@gmail.com>
2021-08-23 14:51:41 +0530

    updated arranging coins

---
+++ Searching/Easy/ArrangingCoins.java
@@ -0,0 +1,18 @@
+// Problem Link : https://leetcode.com/problems/arranging-coins/
+
+class Solution {
+    public int arrangeCoins(int n) {
+        int count = 0, step = 1;
+        while(n > 0) {
+            if((n - step) >= 0) {
+                count++;
+                n -= step;
+                step++;
+            }
+            else {
+                break;
+            }
+        }
+        return count;
+    }
+}
