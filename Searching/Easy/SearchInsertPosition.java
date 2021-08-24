a6fc256fef0869cc4f133cf5e504fc395920f132
Milind Mishra <milind.mishra4@gmail.com>
2021-08-25 01:51:18 +0530

    updated search insert position

---
+++ Searching/Easy/SearchInsertPosition.java
@@ -0,0 +1,20 @@
+// Problem Link : https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, mid = 0, end = nums.length - 1;
+        while(start <= end) {
+            mid = start + (end - start)/2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return nums[mid] < target ? mid + 1 : mid;
+    }
+}
