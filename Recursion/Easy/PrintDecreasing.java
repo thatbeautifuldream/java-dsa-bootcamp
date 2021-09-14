0d3f2ed3694f1a120206e4113911de9625e9feec
Milind Mishra <milind.mishra4@gmail.com>
2021-09-15 01:09:09 +0530

    started recursion

---
+++ Recursion/Easy/PrintDecreasing.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintDecreasing {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printDecreasing(n);
+        scn.close();
+    }
+    
+    public static void printDecreasing(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n - 1);
+    }
+}
