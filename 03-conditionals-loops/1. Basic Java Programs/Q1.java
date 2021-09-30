9683151fce0a1c7f418936a666b7373cc2006e9c
vanshika sharma <85174042+viroopaksh-utrayak@users.noreply.github.com>
2021-09-30 19:26:29 +0530

    Update Q1.java

--- 03-conditionals-loops/1. Basic Java Programs/Q1.java
+++ 03-conditionals-loops/1. Basic Java Programs/Q1.java
@@ -1,12 +1,10 @@
-import java.util.*;
-public class Q1{
-    public static void main(String[] args){
-    // area of circle
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter the radius : ");
-    double radius = input.nextDouble();
-    double area = 3.14 * radius * radius;
-    System.out.println("Area of circle with radius " + radius + " units is " + area + " unit squared.");
-    input.close();
+import java.util.Scanner;
+public class area_circle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);       
+        System.out.println("enter the circle radius: ");
+        float radius = in.nextFloat();
+        System.out.println("the area of circle is: " + (3.14 * radius * radius));
+        in.close();
     }
 }
