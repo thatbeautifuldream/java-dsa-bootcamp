2762a3c6826899949109618bb6b7711099efc5e6
mohit deshwal <mohitdeshwal1999@gmail.com>
2021-10-05 23:11:41 +0530

    Update Q13.java
    you don't need height and width in perimeter of Rhombus
    as perimeter of rhombus and Square is same because all sides of Rhombus are equal
    also you forgot to add semicolon after in.close()

--- 03-conditionals-loops/1. Basic Java Programs/Q13.java
+++ 03-conditionals-loops/1. Basic Java Programs/Q13.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 public class area_Rhombus {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
-        System.out.println("enter the height of Rhombus: ");
-        float height= in.nextFloat();
-        System.out.println("enter the width of Rhombus: ");
-        Float width= in.nextFloat();
-        System.out.println("ans is: " + ((height*width)/2));
-        in.close()
+        System.out.println("enter the side of Rhombus: ");
+        float side= in.nextFloat();
+        System.out.println("ans is: " + (side*4));
+        in.close();
     }
 }
