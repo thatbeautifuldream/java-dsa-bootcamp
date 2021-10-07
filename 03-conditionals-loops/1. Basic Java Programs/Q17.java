778d9598f6bfc8c0755cdeffbd3eade54944cb36
mohit deshwal <mohitdeshwal1999@gmail.com>
2021-10-07 13:01:01 +0530

    Update Q17.java
    You were taking input volume which isn't necessary 
    You were doing 4/3*pi*radius*radius*radius
    Which was giving wrong answer 
    It should be 4*pi*radius*radius*radius

--- 03-conditionals-loops/1. Basic Java Programs/Q17.java
+++ 03-conditionals-loops/1. Basic Java Programs/Q17.java
@@ -5,9 +5,7 @@ public class Q17 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the radius of sphere");
         double radius = sc.nextDouble();
-        System.out.println("Enter the volume of sphere");
-        double volume = sc.nextDouble();
-        System.out.println("The volume of sphere is " + (volume * 4/3 * Math.PI * radius * radius * radius));
+        System.out.println("The volume of sphere is " + (4 * Math.PI * radius * radius * radius)/3);
         sc.close();
     }
 }
