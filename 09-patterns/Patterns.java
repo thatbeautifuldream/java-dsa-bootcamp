c2a397936b0cf02dc7a7726bfb5bc8d412355e8a
Kranthi Guribilli <79071810+Kranthi-Guribilli@users.noreply.github.com>
2021-11-19 10:33:59 +0530

    Update Patterns.java

--- 09-patterns/Patterns.java
+++ 09-patterns/Patterns.java
@@ -259,5 +259,229 @@ for(int i=1;i<=5;i++){
 	}
 System.out.println("----------------------------------------------------------------"); 
 	    
+/* Print the pattern
 
+14.  *********
+      *     *
+       *   *
+        * *
+         *
+*/
 	    
+for(int i=5;i>=1;i--){
+		//Print trailing spaces
+		for(int j=i;j<5;j++){
+			System.out.print(" ");
+		}
+		//Print hollow pyramid
+		for(int j=1;j<=(2*i-1);j++){
+			if(i==5||j==1||j==(2*i-1)){
+				System.out.print("*");
+			}
+			else{
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}	   
+System.out.println("----------------------------------------------------------------"); 
+	    
+/* Print the pattern
+
+15.      *
+        * *
+       *   *
+      *     *
+     *       *
+      *     *
+       *   *
+        * *
+         *
+*/
+ 
+for(int i=1;i<=5;i++){
+		//Print trailing spaces
+		for(int j=i;j<5;j++){
+			printf(" ");
+		}
+		//Print hollow pyramid
+		for(int j=1;j<=(2*i-1);j++){
+			if(j==1||j==(2*i-1)){
+				System.out.print("*");
+			}
+			else{
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	for(int i=5-1;i>=1;i--){
+		//Print trailing spaces
+		for(int j=i;j<5;j++){
+			printf(" ");
+		}
+		//Print hollow pyramid
+		for(int j=1;j<=(2*i-1);j++){
+			if(j==1||j==(2*i-1)){
+				System.out.print("*");
+			}
+			else{
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}	    
+System.out.println("----------------------------------------------------------------"); 
+	    
+/* Print the pattern
+
+16.           1
+            1   1
+          1   2   1
+        1   3   3   1
+      1   4   6   4   1
+*/
+
+int coef=1;
+for (int i = 0; i < 5; i++) {
+      for (int space = 1; space <= 5 - i; space++)
+         System.out.print("  ");
+      for (int j = 0; j <= i; j++) {
+         if (j == 0 || i == 0)
+            coef = 1;
+         else
+            coef = coef * (i - j + 1) / j;
+         System.out.printf("%4d", coef);
+      }
+      System.out.println();
+   }	
+	    
+System.out.println("----------------------------------------------------------------"); 
+	    
+/* Print the pattern
+
+17.      1
+        212
+       32123
+      4321234
+       32123
+        212
+         1
+*/
+
+ for(int i=1;i<=5-1;i++){
+		for(int j=1;j<=5-i;j++){
+			System.out.print(" ");
+		}
+		for(int k=i;k>=1;k--){
+			System.out.printf("%d",k);
+		}
+		for(int k=2;k<=i;k++){
+			System.out.printf("%d",k);
+		}
+		System.out.println();
+	}
+	for(int i=5-2;i>=0;i--){
+		for(int j=1;j<=5-i;j++){
+			System.out.print(" ");
+		}
+		for(int k=i;k>=1;k--){
+			System.out.printf("%d",k);
+		}
+		for(int k=2;k<=i;k++){
+			System.out.printf("%d",k);
+		}
+		System.out.println();
+	}
+System.out.println("----------------------------------------------------------------"); 
+	    
+/* Print the pattern
+
+18.   **********
+      ****  ****
+      ***    ***
+      **      **
+      *        *
+      *        *
+      **      **
+      ***    ***
+      ****  ****
+      **********
+*/
+	    
+//upper half of the pattern
+	for(int i=0;i<5;i++){
+		for(int j=0;j<(2*5);j++){
+			if(i+j<=5-1)//upper left triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+			if((i+5)<=j)//upper right triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+		}
+		System.out.println();
+	}
+	//bottom half of the triangle
+	for(int i=0;i<5;i++){
+		for(int j=0;j<(2*5);j++){
+			if(i>=j)//bottom left triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+			if(i>=(2*5-1)-j)//bottom right triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+		}
+		System.out.println();
+	}   
+System.out.println("----------------------------------------------------------------"); 
+	    
+/* Print the pattern
+
+19.    *        *
+       **      **
+       ***    ***
+       ****  ****
+       **********
+       ****  ****
+       ***    ***
+       **      **
+       *        *
+*/
+
+//upper half of the pattern
+	for(int i=0;i<5;i++){
+		for(int j=0;j<(2*5);j++){
+			if(i>=j)//upper left triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+			if(i>=(2*5-1)-j)//upper right triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+		}
+		System.out.println();
+	}
+	//bottom half of the pattern
+	for(int i=1;i<5;i++){
+		for(int j=0;j<(2*5);j++){
+			if(i+j<=5-1)//bottom left triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+			if((i+5)<=j)//bottom right triangle
+				System.out.print("*");
+			else
+				System.out.print(" ");
+		}
+		System.out.println();
+	}
+System.out.println("----------------------------------------------------------------"); 
+	    
+/* Print the pattern
+
+
