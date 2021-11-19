e4d60137a52c2f604199e8577f1bdbb41930a6a2
Kranthi Guribilli <79071810+Kranthi-Guribilli@users.noreply.github.com>
2021-11-19 10:06:11 +0530

    Update Patterns.java

--- 09-patterns/Patterns.java
+++ 09-patterns/Patterns.java
@@ -240,5 +240,24 @@ System.out.println("------------------------------------------------------------
       *     *
      *********
 */
+ 
+for(int i=1;i<=5;i++){
+		//Print trailing spaces
+		for(int j=i;j<5;j++){
+			System.out.print(" ");
+		}
+		//Print hollow pyramid
+		for(int j=1;j<=(2*i-1);j++){
+			if(int i==5||j==1||j==(2*i-1)){
+				System.out.print("*");
+			}
+			else{
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+System.out.println("----------------------------------------------------------------"); 
 	    
+
 	    
