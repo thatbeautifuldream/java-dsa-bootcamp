ceba2ed4d565642c9980b4e45df8898f2b56761f
Milind Mishra <milind.mishra4@gmail.com>
2021-11-20 18:18:33 +0530

    changes

--- 10-recursion/Easy/AllIndicesInArray.java
+++ 10-recursion/Easy/AllIndicesInArray.java
@@ -9,6 +9,7 @@ public class AllIndicesInArray {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
+        
         int x = Integer.parseInt(br.readLine());
         int[] iarr = allIndices(arr, x, 0, 0);
         if(iarr.length == 0) {
