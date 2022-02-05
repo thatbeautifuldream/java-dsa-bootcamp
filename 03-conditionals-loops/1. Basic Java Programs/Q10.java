6bf79662b49dde7b2a80889edae58ee23ec60986
SaarthakMaini <94912101+SaarthakMaini@users.noreply.github.com>
2022-02-05 19:20:14 +0530

    Small changes
    Improved wording for a better presentation of the program. Hope I was able to add some value!!

--- 03-conditionals-loops/1. Basic Java Programs/Q10.java
+++ 03-conditionals-loops/1. Basic Java Programs/Q10.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class area_Parallelogram {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
-        System.out.println("enter the base of Parallelogram: ");
+        System.out.println("Enter the base of Parallelogram: ");
         float base= in.nextFloat();
-        System.out.println("enter the height of Parallelogram: ");
-        float height= in.nextFloat();
-        System.out.println("ans is: " +2*(base+height));
+        System.out.println("Enter the side of Parallelogram: ");
+        float side= in.nextFloat();
+        System.out.println("The perimeter of the parallelogram is : " +2*(base+side));
     }
 }
